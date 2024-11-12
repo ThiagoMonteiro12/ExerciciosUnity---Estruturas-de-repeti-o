080d598a6711dd62ebd42a9db2103330455d91b4
ThiagoMonteiro12 <thiagoben3@gmail.com>
2024-11-12 14:44:35 -0300

    Feat: Ex05

---
+++ Assets/Exercicio 4.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exercicio4 : MonoBehaviour
+{
+    int pontuacao;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 7; i++)
+        {
+            pontuacao = 10;
+        }
+        print(pontuacao);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
